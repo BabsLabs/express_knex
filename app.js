934b53e12652ce339cfa9b6638565d10753af8b2
Brian Bower <brian@brianbower.com>
2020-01-23 11:39:12 -0700

    Add '/api/v1/papers' endpoint

--- app.js
+++ app.js
@@ -2,8 +2,29 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Publications';
+
+app.get('/', (req, res) => res.send('Hello Publications!'));
+
+app.listen(app,get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
+
+app.get('api/v1/papers', (req, res) => {
+  database('papers').select()
+  .then((papers) => {
+    res.status(200).json(papers);
+  })
+  .catch((error) => {
+    res.status(500).json({ error });
+  });
+});
